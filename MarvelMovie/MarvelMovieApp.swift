d6c66a674964598276a1f624b16b2651d95adb1b
erangaprabath <erangaprabath@icloud.com>
2024-08-31 11:46:59 +0530

    Initial Commit

---
+++ MarvelMovie/MarvelMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelMovieApp.swift
+//  MarvelMovie
+//
+//  Created by Eranga Prabath on 2024-08-31.
+//
+
+import SwiftUI
+
+@main
+struct MarvelMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
