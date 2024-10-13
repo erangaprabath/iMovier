8598ad3432d5d1710e918a988d13b1baf0ed6223
erangaprabath <erangaprabath@icloud.com>
2024-10-13 14:06:30 +0530

    added video player view model and view to the project

---
+++ MarvelMovie/Core/VideoPlayer/View/VideoPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerView.swift
+//  MarvelMovie
+//
+//  Created by Eranga Prabath on 2024-10-13.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlayerView()
+}
