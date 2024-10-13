a791e0f01b8933dc097039d9cc507afb84d26aef
erangaprabath <erangaprabath@icloud.com>
2024-10-13 14:09:42 +0530

    test

--- MarvelMovie/Core/VideoPlayer/View/VideoPlayerView.swift
+++ MarvelMovie/Core/VideoPlayer/View/VideoPlayerView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct VideoPlayerView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("Video player")
+        Text("Video")
     }
 }
 
